2c8ada76879cbc3ede6f5bc4ee5fe5da48aac462
starryskies <tkhjh100@gmail.com>
2023-03-12 10:45:06 +0200

    .

--- src/App.js
+++ src/App.js
@@ -1,9 +1,17 @@
+/** @format */
+
+import react from "react-dom";
+
 function App() {
   return (
     <div>
+      <img
+        src="https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcSY1E-2nBHPSM66x0lvjlS5xo4q5aBYrXYrdnXozVwm&s"
+        alt="banana"
+      />
       <h2>welcome to my portfolio</h2>
+      <h1>my name is elichay ben zohar,im 25yo and im learning react</h1>
     </div>
-  <button></button>
   );
 }
 
